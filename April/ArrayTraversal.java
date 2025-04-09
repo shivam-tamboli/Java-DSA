09f43d0d9bde3631691811fb42ac96728b60d62b
Shivam <shivamtamboli66@gmail.com>
2025-04-09 21:19:09 +0530

    Traversing into an array accessing every element an array

---
+++ April/ArrayTraversal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/*
+Expected input will be:
+	arr[] = {1,2,3,4,5,6,7};
+*/
+
+public class ArrayTraversal{
+
+	public static void main(String[] args){
+	
+	int arr[] = {1,2,3,4,5,6,7};
+
+	System.out.println("Traversing into the array:");
+
+	for(int i = 0; i < arr.length; i++){
+	System.out.println(arr[i] + " ");
+		}
+	}
+}
+
+/*
+Expected output will be:
+Traversing into the array:
+1 
+2 
+3 
+4 
+5 
+6 
+7 
+*/

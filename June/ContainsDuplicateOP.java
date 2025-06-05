675fd51039ff21354e7ca6c18bc320f21b48e0f5
Shivam <shivamtamboli66@gmail.com>
2025-06-05 21:41:56 +0530

    optimized code with appropirate Data Structure and using  oops concepts

--- June/ContainsDuplicateOP.java
+++ June/ContainsDuplicateOP.java
@@ -19,23 +19,33 @@ Output = true.
 import java.util.*;
 
 public class ContainsDuplicateOP{
+
+	public static boolean hasDuplicate(int[] nums){
+	
+	HashSet<Integer> seen = new HashSet<>();
+	
+	for(int num : nums){
+	if(seen.contains(num)){
+		return true;
+		//break;
+			}
+	seen.add(num);
+		}
+	return false;
+	
+	}
+	
+	
 	
 	public static void main(String[] args){
 
 	int[] nums = {1,2,3,3};//an array.
+
+	boolean result = hasDuplicate(nums);
 	
-	HashSet<Integer> seen = new HashSet<>();//create a set that doesn't allow duplicates.
-
-	boolean duplicate = false;//create a flag.
-
-	for(int num : nums){                       //iterating in an array.
-		if(seen.contains(num)){             //if num repeates in set.
-			duplicate = true;             //change the flag.
-			break;                       	//break the loop.
-			}					//if their is no duplicate.
-		seen.add(num);				//add the number in set.
-		}
-	System.out.println(duplicate);		//print the flag.
+	System.out.println(result);
+	
+	
 	}
 }
 

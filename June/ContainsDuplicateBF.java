0ecd2f34b25a288019cfc4b575285a9fb94d89c1
Shivam <shivamtamboli66@gmail.com>
2025-06-05 15:11:49 +0530

    modify

--- June/ContainsDuplicateBF.java
+++ June/ContainsDuplicateBF.java
@@ -1,3 +1,19 @@
+/*
+NeetCode
+
+Contains Duplicate
+
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Input : nums = {1,2,3,4};
+Output : flase.
+
+
+Input : nums = {4,5,6,1,1};
+Output = true.
+*/
+
+
 import java.util.*;
 
 
@@ -23,3 +39,11 @@ for(int i = 0; i < nums.length; i++){//outloop for iterating into array.
 	}
 
 }
+
+/*
+Expected Input will be:
+int[] nums = {1,2,3,3}
+
+Expected Output will be:
+true
+*/
